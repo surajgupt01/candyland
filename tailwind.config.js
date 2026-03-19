f86cfa5bd7f77bd5016edbdf56d717ef6fa3e522
surajgupt01 <surajgupt880@gmail.com>
2026-03-19 14:07:30 +0530

    font style added

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        thunder: ['Thunder', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
